8d4146039f6372b0a77fcbcfa859b26cd6225ff0
Niyonkuru Barack <niyobarack001@gmail.com>
2026-08-08 23:01:50 +0200

    Improve project 1 greeting

--- 01-personal-greeting/main.py
+++ 01-personal-greeting/main.py
@@ -1,11 +1,11 @@
-def greating(name,age,country):
+def greeting(name,age,country):
     print("Hello ",name.capitalize())
     print(f'You are {age} years old ')
-    print("You are from",country.upper())
+    print("You are from",country.title())
     print("Welcome to Python programming!")
 
 
 name = input("What is your name: ")
 age = int(input ("What is your age: "))
 country = input ("What is your country: ")
-greating(name,age,country)
+greeting(name,age,country)
